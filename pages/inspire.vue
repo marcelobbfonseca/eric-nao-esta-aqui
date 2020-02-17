cbfc911a4bdc50e78c13d7969154d29efc68741f
marcelobbfonseca <marcelobbff@gmail.com>
2020-02-17 10:08:37 -0300

    inspiring message

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220; Just a heads-up: That coffee we gave you earlier had fluorescent calcium in it so we can track the neuronal activity in your brain. There's a slight chance the calcium could harden and vitrify your frontal lobe. Anyway, don't stress yourself thinking about it. I'm serious. Visualizing the scenario while under stress actually triggers the reaction.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;Cave Johnson</em>
           </small>
         </footer>
       </blockquote>
